a24d31c80c077cf4faf73116bf856c7fe695f5c2
이솔 <lees@focusmediakorea.com>
2023-02-22 14:33:08 +0900

    comment: move comment

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ const App = () => (
           <div>Framework: react</div>
           <div>Language: TypeScript</div>
           <div>CSS: Tailwind</div>
+          <div>
+              <p>이 프로젝트에서는 Recipe.tsx와 Recipes.tsx를 정의하고 expose 합니다.</p>
+              <p>이 프로젝트에서는 해당 컴포넌트들을 화면에 불러오고 있지 않으므로 현재 도메인에서는 정보를 볼 수 없습니다.</p>
+          </div>
       </div>
   </>
 );
